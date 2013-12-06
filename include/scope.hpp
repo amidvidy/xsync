fc3cba56e39b61045bac24e0ec79741ecb84afb6
= <amidvidy@gmail.com>
2013-12-05 16:01:15 -0800

    start scope

--- include/scope.hpp
+++ include/scope.hpp
@@ -1,3 +1,17 @@
+#include "lock.hpp"
+
 namespace xsync {
-  
+
+/*
+ * Provides an RAII style wrapper for transactional execution.
+ */
+template <typename LockType>
+class Scope {
+public:
+    Scope(LockType& fallback, bool writeLock = false);
+    ~Scope();
+private :
+    LockType &fallback_;
+};
+
 }
